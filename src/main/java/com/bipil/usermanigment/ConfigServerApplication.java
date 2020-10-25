6bc4cd974ec87f17e123f594cc9d8e06f05648b2
Bipil Raut <bipilraut@gmail.com>
2020-10-25 17:04:32 +0530

    Eureka Server code committed

---
+++ src/main/java/com/bipil/usermanigment/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.bipil.usermanigment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bipil/usermanigment/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bipil.usermanigment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
